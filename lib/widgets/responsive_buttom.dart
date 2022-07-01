e0412edc55f2f770e578f1c24a47293ebd14df73
piyushyadavNP <yadavpiyushkp@gmail.com>
2022-07-01 15:57:14 +0545

    Welcome Page Completed

---
+++ lib/widgets/responsive_buttom.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/widgets.dart';
+import 'package:travel_app/misc/colors.dart';
+
+class ResponsiveButton extends StatelessWidget {
+  bool? isResponsive;
+  double? width;
+  ResponsiveButton({Key? key, this.isResponsive = false, this.width})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: 60,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10), color: AppColors.mainColor),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("assets/images/button-one.png"),
+        ],
+      ),
+    );
+  }
+}
